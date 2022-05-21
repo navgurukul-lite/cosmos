570d2e49d7126285296bd95f848bdd24b35eeb6e
Pratik <pratikdeshmukhlobhi@gmail.com>
2022-05-21 14:32:11 +0530

    about changes

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -3,92 +3,84 @@ import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
 import Hero from "../../Components/Team";
 import Projects from "../../Components/Projects";
-import campuses from "../../Data/campuses.json";
+import active_projects from "../../Data/active_projects.json";
 
 const index = () => {
   return (
     <div>
       <Navbar />
       <section class="text-gray-400 bg-gray-900 body-font">
-        <div class="container mx-auto flex px-5 py-20 items-center justify-center flex-col">
+        <div class="container mx-auto flex py-12 items-center justify-center flex-col">
           <img
             class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded animate__animated animate__backInUp animate__slower"
             alt="hero"
             src="https://github.com/thompsonemerson/thompsonemerson/raw/master/cover-thompson.png"
           />
           <div class="text-center lg:w-2/3 w-full">
-            <h1 class="title-font sm:text-2xl text-3xl mb-4 font-medium text-white animate__animated animate__backInUp animate__slower">
-              Navgurukul Lite Managed by Navgurukul Students
+            <h1 class="title-font  mb-4 ml-2 font-medium text-white animate__animated animate__backInUp animate__slower">
+              <span className="text-2xl font-bold  text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-orange-600">
+                Navgurukul{" "}
+                <span className="text-base hover:animate-spin mr-1">Lite</span>
+              </span>
+              Managed by Navgurukul Students
             </h1>
-            <p class="leading-relaxed mb-8 animate__animated animate__backInUp animate__slower">
-              We welcome you all let's code the future and makes our dreams come true
+            <p class="leading-relaxed animate__animated animate__backInUp animate__slower">
+              We welcome you all let's code the future and makes our dreams come
+              true
             </p>
           </div>
         </div>
       </section>
 
       <section class="text-gray-400 bg-gray-900 body-font">
-        <div class="container px-5 py-16 mx-auto">
-          <h1 class="text-3xl font-medium title-font text-white mb-12 text-center animate__animated animate__rollIn animate__slow">
-            Our Campuses
+        <div class="container px-5 mx-auto">
+          <h1 class="text-3xl font-medium title-font text-white mb-5 text-center animate__animated animate__rollIn animate__slow">
+            Active Projects
+            <div class="flex py-3 justify-center">
+              <div
+                style={{ height: "0.4px" }}
+                class="w-32 rounded-full bg-red-500 inline-flex"
+              ></div>
+            </div>
           </h1>
 
-          <div class="flex flex-wrap -m-4">
-            {campuses.map((item) => {
-              return (
-                <div class="p-4 md:w-1/3 animate__animated animate__backInUp animate__slow">
-                  <div class="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-                    <img
-                      class="lg:h-48 md:h-36 w-full object-cover object-center"
-                      src={item.image}
-                      alt="blog"
-                    />
-                    <div class="p-6">
-                      <h1 class="title-font text-lg font-medium text-white mb-3">
-                        {item.name}
-                      </h1>
-                      <div class="flex items-center flex-wrap ">
+          <section class="text-gray-400 bg-gray-900 body-font">
+            <div class="container px-5 py-5 mx-auto">
+              <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+                {active_projects.map((item) => {
+                  return (
+                    <div class="p-4 w-full md:w-1/3 flex flex-col text-center items-center animate__animated animate__backInUp animate__slower">
+                      <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-gray-800 text-red-400 mb-5 flex-shrink-0">
+                        <img src={item.image} />
+                      </div>
+                      <div class="flex-grow">
+                        <h2 class="text-white text-lg title-font font-medium mb-3">
+                          {item.name}
+                        </h2>
+                        <p class="leading-relaxed text-base">
+                          {item.description}
+                        </p>
                         <a
-                          href={item.map}
-                          class="text-red-400 inline-flex items-center md:mb-2 lg:mb-0"
+                          href={item.demo}
+                          class="mt-3 text-red-400 inline-flex items-center mr-4"
                         >
-                          View On Map
-                          <svg
-                            class="w-4 h-4 ml-2"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            fill="none"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                          >
-                            <path d="M5 12h14"></path>
-                            <path d="M12 5l7 7-7 7"></path>
-                          </svg>
+                          Demo
+                          
                         </a>
-                        <span class="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-800">
-                          <svg
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            class="text-red-400 w-4 h-4 mr-2 inline-block"
-                            viewBox="0 0 24 24"
-                          >
-                            <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
-                            <circle cx="9" cy="7" r="4"></circle>
-                            <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
-                          </svg>
-                          {item.students}
-                        </span>
+                        <a
+                          href={item.github}
+                          class="mt-3 text-red-400 inline-flex items-center"
+                        >
+                          Gitub
+                          
+                        </a>
                       </div>
                     </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+                  );
+                })}
+              </div>
+            </div>
+          </section>
         </div>
       </section>
       <Footer />
